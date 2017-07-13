57fcca51321565baf21187325f82ec63b6dac2ca
filip <bitbucket@xn--feld-4qa.de>
2017-07-13 16:10:18 +0200

    program now has needed assemblies embedded

--- Duathlon/App.xaml.cs
+++ Duathlon/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,20 @@ namespace Duathlon
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                string resourceName = $"Duathlon.dll.{new AssemblyName(args.Name).Name}.dll";
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+        }
     }
 }
